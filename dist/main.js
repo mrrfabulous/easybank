81f66afac196fc4441829d50df65a3608b68a548
Joseph <mrrfabulous8@gmail.com>
2025-01-20 21:58:08 +0100

    vercel setup

---
+++ dist/main.js
@@ -0,0 +1,14 @@
+let hambuger = document.querySelector(".hambuger")
+let closeBtn = document.querySelector(".close")
+let nav = document.querySelector(".nav-list")
+
+
+let toggleActive = () => {
+    nav.classList.toggle("active")
+    // alert("I have been clicked")
+}
+
+
+hambuger.addEventListener("click", toggleActive )
+nav.addEventListener("click", toggleActive )
+
